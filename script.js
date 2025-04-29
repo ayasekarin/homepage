bc542d323ed008cae44bfb3991bffb6bcc78ca4e
ayasekarin <syushin0812@gmail.com>
2025-04-29 16:00:59 +0900

    20250429_video追加

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 AOS.init();
+
+// 短片播放结束后显示ENTRY按钮
+const introMovie = document.getElementById('introMovie');
+const entryButton = document.getElementById('entryButton');
+
+introMovie.addEventListener('ended', () => {
+  introMovie.loop = true; // 结束后设置循环播放
+  introMovie.play();
+  entryButton.style.display = 'block'; // 显示ENTRY按钮
+});
+
+// 点击ENTRY按钮，隐藏intro-video，显示主页
+function enterSite() {
+  document.getElementById('intro-video').style.display = 'none';
+}
