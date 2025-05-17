51a7b45f20c164670c973a2c46e717359b086ced
ayasekarin <syushin0812@gmail.com>
2025-05-17 13:46:43 +0900

    20250517_ナビゲーター横式修正

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 AOS.init();
 
-// 短片播放结束后显示ENTRY按钮
 const introMovie = document.getElementById('introMovie');
 const entryButton = document.getElementById('entryButton');
 
 introMovie.addEventListener('ended', () => {
-  introMovie.loop = true; // 结束后设置循环播放
+  introMovie.loop = true;
   introMovie.play();
-  entryButton.style.display = 'block'; // 显示ENTRY按钮
+  entryButton.style.display = 'block';
 });
 
-// 点击ENTRY按钮，隐藏intro-video，显示主页
 function enterSite() {
   document.getElementById('intro-video').style.display = 'none';
 }
