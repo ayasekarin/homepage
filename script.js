17225a5af7593f644615910a4278d8c8ee6e970e
ayasekarin <syushin0812@gmail.com>
2025-05-17 15:11:28 +0900

    20250517_homePageデザイン修正

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-AOS.init();
 
 const introMovie = document.getElementById('introMovie');
 const entryButton = document.getElementById('entryButton');
 
+//--------------------------------------------------------------
+// 開幕動画の再生
+//--------------------------------------------------------------
 introMovie.addEventListener('ended', () => {
   introMovie.loop = true;
   introMovie.play();
@@ -11,4 +13,14 @@ introMovie.addEventListener('ended', () => {
 
 function enterSite() {
   document.getElementById('intro-video').style.display = 'none';
+
+  window.scrollTo(0, 0);
+
+  // AOSをここで初期化
+  setTimeout(() => {
+    AOS.init({
+      once: true, 
+    });
+  }, 100);
+
 }
